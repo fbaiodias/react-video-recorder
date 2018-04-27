9bb98f047d09422c77ff3fb8c0f13d4844cf06a5
David Okuniev <davidokuniev@Davids-MacBook-Pro-2.local>
2018-04-27 13:29:17 +0200

    feat: tweaked position of timer

--- src/defaults/timer.js
+++ src/defaults/timer.js
@@ -3,8 +3,8 @@ import styled from 'styled-components'
 
 const Text = styled.div`
   position: absolute;
-  top: 20px;
-  right: 20px;
+  top: 50px;
+  right: 50px;
   font-family: Menlo, monospace;
   font-size: 28px;
   text-shadow: 1px 2px rgba(0, 0, 0, 0.5);
