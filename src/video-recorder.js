2949e436a6cb6239087105e79560ae120c14df79
Francisco Baio Dias <francisco@typeform.com>
2018-11-27 22:28:33 +0100

    fix: support iOS chrome

--- src/video-recorder.js
+++ src/video-recorder.js
@@ -71,7 +71,7 @@ const isIOSSafari = () => {
   const ua = window.navigator.userAgent
   const iOS = !!ua.match(/iPad/i) || !!ua.match(/iPhone/i)
   const webkit = !!ua.match(/WebKit/i)
-  return iOS && webkit && !ua.match(/CriOS/i)
+  return iOS && webkit
 }
 
 const captureThumb = videoTag =>
