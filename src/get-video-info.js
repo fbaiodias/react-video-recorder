382d122b53c44e2a8b1b52521865b95a50ff18e4
Avinash Nishad <nishad.premnarayan@nagarro.com>
2020-08-04 17:20:55 +0530

    fix(Linitng): Linting isssue fix

--- src/get-video-info.js
+++ src/get-video-info.js
@@ -1,81 +1,82 @@
-export const captureThumb = (videoTag) =>
+export const captureThumb = videoTag =>
   new Promise((resolve, reject) => {
-    const canvas = document.createElement("canvas");
-    canvas.width = videoTag.videoWidth;
-    canvas.height = videoTag.videoHeight;
-    canvas.getContext("2d").drawImage(
+    const canvas = document.createElement('canvas')
+    canvas.width = videoTag.videoWidth
+    canvas.height = videoTag.videoHeight
+    canvas.getContext('2d').drawImage(
       videoTag,
       0, // top
       0, // left
       videoTag.videoWidth,
       videoTag.videoHeight
-    );
-    canvas.toBlob((thumbnail) => {
-      resolve(thumbnail);
-    }, "image/jpeg");
-  });
+    )
+    canvas.toBlob(thumbnail => {
+      resolve(thumbnail)
+    }, 'image/jpeg')
+  })
 
-const getVideoInfo = (videoBlob) =>
+const getVideoInfo = videoBlob =>
   new Promise((resolve, reject) => {
-    const videoTag = document.createElement("video");
-    videoTag.preload = "metadata";
-    videoTag.muted = true;
-    videoTag.defaultMuted = true;
-    videoTag.playsInline = true;
-    videoTag.autoplay = true;
+    const videoTag = document.createElement('video')
+    videoTag.preload = 'metadata'
+    videoTag.muted = true
+    videoTag.defaultMuted = true
+    videoTag.playsInline = true
+    videoTag.autoplay = true
 
-    let resolved = false;
+    let resolved = false
 
     const handleTimeout = () => {
-      resolved = true;
+      resolved = true
       resolve({
         duration: null,
-        thumbnail: null,
-      });
+        thumbnail: null
+      })
       videoTag.removeEventListener &&
-        videoTag.removeEventListener("loadeddata", handleLoadedData);
-      window.URL.revokeObjectURL(videoTag.src);
-    };
+        videoTag.removeEventListener('loadeddata', handleLoadedData)
+      window.URL.revokeObjectURL(videoTag.src)
+    }
 
-    const timeout = setTimeout(handleTimeout, 1000);
+    const timeout = setTimeout(handleTimeout, 1000)
 
-    const handleVideoTag = (duration) => {
+    const handleVideoTag = duration => {
       captureThumb(videoTag)
-        .then((thumbnail) => {
-          videoTag.pause();
+        .then(thumbnail => {
+          videoTag.pause()
           if (!resolved) {
-            clearTimeout(timeout);
-            resolved = true;
-            resolve({ duration, thumbnail });
+            clearTimeout(timeout)
+            resolved = true
+            resolve({ duration, thumbnail })
           }
-          window.URL.revokeObjectURL(videoTag.src);
+          window.URL.revokeObjectURL(videoTag.src)
         })
-        .catch((err) => {
+        .catch(err => {
           if (!resolved) {
-            clearTimeout(timeout);
-            resolved = true;
-            reject(err);
+            clearTimeout(timeout)
+            resolved = true
+            reject(err)
           }
-        });
-    };
+        })
+    }
 
     const handleLoadedData = () => {
-      let duration = videoTag.duration * 1000;
+      let duration = videoTag.duration * 1000
       if (videoTag.duration === Infinity) {
-        videoTag.currentTime = Number.MAX_SAFE_INTEGER;
+        videoTag.currentTime = Number.MAX_SAFE_INTEGER
         videoTag.ontimeupdate = () => {
-          videoTag.ontimeupdate = null;
-          duration = videoTag.duration * 1000;
-          videoTag.currentTime = 0;
-          handleVideoTag(duration);
-        };
+          videoTag.ontimeupdate = null
+          duration = videoTag.duration * 1000
+          videoTag.currentTime = 0
+          handleVideoTag(duration)
+        }
       } else {
-        handleVideoTag(duration);
+        handleVideoTag(duration)
       }
-    };
+    }
 
-    videoTag.addEventListener("loadeddata", handleLoadedData);
-    videoTag.src = window.URL.createObjectURL(videoBlob);
-  });
+    videoTag.addEventListener('loadeddata', handleLoadedData)
+    videoTag.src = window.URL.createObjectURL(videoBlob)
+  })
 
-export default getVideoInfo;
+export default getVideoInfo
+
